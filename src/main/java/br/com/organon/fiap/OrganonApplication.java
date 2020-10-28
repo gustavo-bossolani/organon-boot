eaff2e9fdc13332bf3631bcd5eb38b46d531ceaa
gustavo-bossolani <gustavo.wyd@outlook.com>
2020-10-27 22:32:31 -0300

    :tada: :racehorce: projeto base

---
+++ src/main/java/br/com/organon/fiap/OrganonApplication.java
@@ -0,0 +1,13 @@
+package br.com.organon.fiap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrganonApplication.class, args);
+    }
+
+}
