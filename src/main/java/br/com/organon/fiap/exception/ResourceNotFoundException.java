9223d5c4a09d37bd0f675834b0a311ed33639ff7
gustavo-bossolani <gustavo.wyd@outlook.com>
2020-10-28 23:54:13 -0300

    :rocket: cria exception de resource not found e resource already populated

---
+++ src/main/java/br/com/organon/fiap/exception/ResourceNotFoundException.java
@@ -0,0 +1,16 @@
+package br.com.organon.fiap.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException() {
+        super("Recurso não encontrado.");
+    }
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+}
